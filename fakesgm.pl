b1aecc7baf0304474603cf57c7a77b5168417f3a
Graham Neubig <neubig@gmail.com>
2012-05-12 11:09:44 +0900

    Added interleave and fake sgm scripts

---
+++ fakesgm.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+use strict;
+use utf8;
+use List::Util qw(sum min max shuffle);
+use Getopt::Long;
+binmode STDIN, ":utf8";
+binmode STDOUT, ":utf8";
+binmode STDERR, ":utf8";
+
+if(@ARGV != 2) {
+    print STDERR "Usage: fakesgm.pl src/ref tar\n";
+    exit 1;
+}
+
+print "<".$ARGV[0]."set setid=\"test2006\" srclang=\"any\" trglang=\"$ARGV[1]\">
+<DOC docid=\"test2006\"".(($ARGV[1] eq "ref")?" sysid=\"ref\"":" sysid=\"src\"").">\n";
+
+my $sent = 1;
+while(<STDIN>) {
+    chomp;
+    print "<seg id=".$sent++."> $_ </seg>\n";
+}
+
+print "</DOC>\n</".$ARGV[0]."set>\n";
---
+++ interleave.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+
+use strict;
+use utf8;
+use FileHandle;
+use List::Util qw(sum min max shuffle);
+binmode STDIN, ":utf8";
+binmode STDOUT, ":utf8";
+binmode STDERR, ":utf8";
+
+if(@ARGV == 0) {
+    print STDERR "Usage: FILES";
+    exit 1;
+}
+
+my @handles;
+for(@ARGV) {
+   my $fh = IO::File->new("< $_") or die "didn't work for $_";
+   binmode $fh, ":utf8";
+   push @handles, $fh;
+}
+
+while(1) {
+    for(@handles) {
+        my $val = <$_>;
+        exit if not $val;
+        print $val;
+    }
+    print "\n";
+}
