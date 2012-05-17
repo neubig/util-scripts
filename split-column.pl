a1b98cffa62c084674f49901449f743d3b9ae303
Graham Neubig <neubig@gmail.com>
2012-05-18 07:50:04 +0900

    Added some splitting scripts

---
+++ split-column.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+use strict;
+use utf8;
+use FileHandle;
+use Getopt::Long;
+use List::Util qw(sum min max shuffle);
+binmode STDIN, ":utf8";
+binmode STDOUT, ":utf8";
+binmode STDERR, ":utf8";
+
+if(@ARGV != 1) {
+    print STDERR "Usage: $0\n";
+    exit 1;
+}
+
+while(<STDIN>) {
+    chomp;
+    my @arr = split(/\t/);
+    print "$arr[$ARGV[0]]\n";
+}
--- splittesttrain.pl
+++ splittesttrain.pl
@@ -1,6 +1,18 @@
 #!/usr/bin/perl
 
+use strict;
+use utf8;
+use FileHandle;
+use Getopt::Long;
+use List::Util qw(sum min max shuffle);
 binmode STDIN, ":utf8";
+binmode STDOUT, ":utf8";
+binmode STDERR, ":utf8";
+
+if(@ARGV != 3) {
+    print STDERR "Usage: $0 FRACTION TEST TRAIN\n";
+    exit 1;
+}
 
 my $fraction = $ARGV[0];
 open TEST, ">:utf8", $ARGV[1] or die $!;
@@ -8,6 +20,7 @@ if($ARGV[2]) {
     open TRAIN, ">:utf8", $ARGV[2] or die $!;
 }
 
+my $i = 0;
 while(<STDIN>) {
     if($i++ % $fraction) {
         if($ARGV[2]) {
