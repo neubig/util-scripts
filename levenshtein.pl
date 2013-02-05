81015d91801e33a9ff147372cf35fff0a6f962c7
Tetsuo Kiso <tetsuo-s@is.naist.jp>
2013-02-05 20:39:40 +0900

    Change the type of variables.
    It looks like hash variables were defined as arrays.

--- levenshtein.pl
+++ levenshtein.pl
@@ -5,16 +5,16 @@ use warnings;
 #  d=delete i=insert s=substitute e=equal
 sub levenshtein {
     my ($s, $t) = @_;
-    my (@sc, $m, @tc, $n, %d, @str, $i, $j, $id, $cost, $type, $aid, $bid, $cid, $c);
+    my (@sc, $m, @tc, $n, %d, %str, $i, $j, $id, $cost, $type, $aid, $bid, $cid, $c);
     @sc = split(/ +/, $s);
     $m = @sc;
     @tc = split(/ +/, $t);
     $n = @tc;
     # initialize
-    @d = (); @str = ();
+    %d = (); %str = {};
     foreach $i (0 .. $m) { $id = pack('S2', $i, 0); $d{$id} = $i; $str{$id} = 'd'x$i; }
     foreach $j (1 .. $n) { $id = pack('S2', 0, $j); $d{$id} = $j; $str{$id} = 'i'x$j; }
-    
+
     foreach $i (1 .. $m) {
         foreach $j (1 .. $n) {
             if($sc[$i-1] eq $tc[$j-1]) {
@@ -26,9 +26,9 @@ sub levenshtein {
             $aid = pack('S2', $i-1, $j); $a = $d{$aid} + 1; # deletion
             $bid = pack('S2', $i, $j-1); $b = $d{$bid} + 1; # insertion
             $cid = pack('S2', $i-1, $j-1); $c = $d{$cid} + $cost; # insertion
-            
+
             $id = pack('S2', $i, $j);
-            
+
             # we want matches to come at the end, so do deletions/insertions first
             if($a <= $b and $a <= $c) {
                 $d{$id} = $a;
@@ -52,7 +52,7 @@ sub levenshtein {
     }
 
     $id = pack('S2', $m, $n);
-    return ($str{$id}, $d{$id}); 
+    return ($str{$id}, $d{$id});
 }
 
 1
