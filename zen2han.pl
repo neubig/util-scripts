18f44b8501de4dc13d5e8a15d622105fdc357a0b
Graham Neubig <neubig@gmail.com>
2013-02-14 17:17:44 +0900

    Added a script for combining zenkaku and hankaku

---
+++ zen2han.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use utf8;
+use List::Util qw(sum min max shuffle);
+use Getopt::Long;
+binmode STDIN, ":utf8";
+binmode STDOUT, ":utf8";
+binmode STDERR, ":utf8";
+
+my $NOHYPHEN = 0;
+my $ZENSPACE = 0;
+my $TRIM = 0;
+GetOptions(
+    "nohyphen" => \$NOHYPHEN,
+    "zenspace" => \$ZENSPACE,
+    "trim" => \$TRIM,
+);
+
+if(@ARGV != 0) {
+    print STDERR "Usage: han2zen.pl < INPUT > OUTPUT\n";
+    exit 1;
+}
+
+while(<STDIN>) {
+    chomp;
+    if($TRIM) { s/^ +//g; s/ +$//g; }
+    tr/ａ-ｚＡ-Ｚ０-９（）［］｛｝＜＞　．，＿％「」、”？・＋：。！＆＊/a-zA-Z0-9()[]{}<> .,_%｢｣､"?･+:｡!&*/;
+    s/／/\//g;
+    s/－/-/g if not $NOHYPHEN;
+    s/ /　/g if $ZENSPACE;
+    print "$_\n";
+}
