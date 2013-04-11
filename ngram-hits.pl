4bdcfe785b2ed4d4d01138dfca3f1b73e558bbcb
Graham Neubig <neubig@gmail.com>
2013-04-11 15:31:23 +0900

    Added ngram-hits

---
+++ ngram-hits.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use utf8;
+use Getopt::Long;
+use List::Util qw(sum min max shuffle);
+binmode STDIN, ":utf8";
+binmode STDOUT, ":utf8";
+binmode STDERR, ":utf8";
+
+my $N = 4;
+GetOptions(
+    "n=i" => \$N,
+);
+
+if(@ARGV != 2) {
+    print STDERR "Usage: $0 REF SYS\n";
+    exit 1;
+}
+
+open FILE0, "<:utf8", $ARGV[0] or die "Couldn't open $ARGV[0]\n";
+open FILE1, "<:utf8", $ARGV[1] or die "Couldn't open $ARGV[1]\n";
+
+sub count_ngrams {
+    my %ret;
+    my @arr = split(/ /, $_[0]);
+    foreach my $i (0 .. $#arr) {
+        my @str;
+        foreach my $j ($i .. min($#arr, $i+$N-1)) {
+            push @str, $arr[$j];
+            $ret{"@str"}++;
+        }
+    }
+    return %ret;
+}
+
+my %total;
+my ($s0, $s1);
+while(($s0 = <FILE0>) and ($s1 = <FILE1>)) {
+    chomp;
+    my %n0 = count_ngrams($s0);
+    my %n1 = count_ngrams($s1);
+    for(keys %n0) {
+        if($n1{$_}) {
+            $total{$_} += min($n0{$_}, $n1{$_});
+        }
+    }
+}
+
+for(sort { $total{$b} <=> $total{$a} } keys %total) {
+    print "$_\t$total{$_}\n";
+}
