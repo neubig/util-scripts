da7a91a2291129f76af819100ced68e21ef13118
Graham Neubig <neubig@gmail.com>
2013-07-16 10:49:07 +0900

    Created a script to make a server

---
+++ serverfy.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+
+use strict;
+use utf8;
+use Getopt::Long;
+use RPC::XML::Server;
+use MIME::Base64;
+use File::Basename;
+use List::Util qw(sum min max shuffle);
+use FileHandle;
+use IPC::Open2;
+binmode STDIN, ":utf8";
+binmode STDOUT, ":utf8";
+binmode STDERR, ":utf8";
+
+$RPC::XML::ENCODING = 'utf-8';
+
+my $PORT = 9002;
+GetOptions(
+"port=s" => \$PORT,
+);
+
+if(@ARGV != 1) {
+    print STDERR "Usage: $0\n";
+    exit 1;
+}
+
+my $pid = open2(*Reader, *Writer, $ARGV[0]) or die "Could not run $ARGV[0]\n";
+
+# 関数を追加
+sub run_cmd {
+    my $s = shift; # サーバーオブジェクト
+    my $t = shift; # txt name
+    print STDERR "IN: $t\n";
+    print Writer "$t\n";
+    my $got = <Reader>;
+    chomp $got;
+    print STDERR "OUT: $got\n";
+ 	return $got;
+}
+
+# サーバーの情報を初期化
+my $srv = RPC::XML::Server->new(port => $PORT);
+
+# メソッドを構築
+my $ls_method = RPC::XML::Procedure->new();
+
+# メソッドを追加
+$srv->add_method(
+    { name => 'process',
+      code => \&run_cmd,
+      signature => [ 'string string' ] }
+);
+
+# サーバーを立ち上げる
+$srv->server_loop;
